c50f86a6f6e0f71d1d6e1238b14855842e6ffbc2
Miguel Vélez <34169959+mvelezg99@users.noreply.github.com>
2020-02-25 17:17:59 -0500

    Registro de formulario de Hoja de Vida - Diseño inicial FRONT

--- src/Components/Pages/index.js
+++ src/Components/Pages/index.js
@@ -3,10 +3,11 @@ import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 // components
-import Login from "./Login";
-import Home from "./Home";
 import Header from "../Global/Header";
 
+import Login from "./Login";
+import Home from "./Home";
+import CurriculumRegister from "./CurriculumRegister";
 
 function Pages() {
   return (
@@ -15,6 +16,7 @@ function Pages() {
       <Switch>
         <Route exact path="/home" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/curriculum" component={CurriculumRegister} />
       </Switch>
     </Router>
   );
