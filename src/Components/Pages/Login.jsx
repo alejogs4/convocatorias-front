698a8ce01adf7eaab33542588eae0c201a2510c7
maugarbru <mauriciogarzonb@gmail.com>
2020-02-24 21:11:40 -0500

    Cambios login

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -5,51 +5,61 @@ import {
   Col,
   Button,
   Form,
-  Jumbotron,
-  Image
+  Alert,
+  Card
 } from "react-bootstrap";
 import prometeo from "../../Images/prometeo.jpg";
 
+var sectionStyle = {
+  width: "100%",
+  backgroundImage: `url(${prometeo})`,
+  backgroundRepeat: "no-repeat",
+  backgroundAttachment: "fixed",
+  backgroundSize: "cover"
+};
+
 export default function Login() {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Image src={prometeo} fluid />
-        </Col>
-        <Col>
-          <Jumbotron fluid>
-            <Container>
-              <h1>INICIAR SESIÓN</h1>
-              <p>
-                Es necesario el inicio de sesión para acceder a las
-                convocatorias y demás opciones.
-              </p>
-              <br></br>
-              <Button href="/register" variant="outline-danger" block>
-              No tienes cuenta? REGISTRATE AQUI
-            </Button>
-            </Container>
-          </Jumbotron>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Correo electrónico</Form.Label>
-              <Form.Control type="email" placeholder="ejemplo@udem.co" />
-              <Form.Text className="text-muted">
-                Correo con el que realizó el registro.
-              </Form.Text>
-            </Form.Group>
+    <div style={sectionStyle}>
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col sm={9} md={7} lg={5} mx="auto">
+            <Card my={5}>
+              <Card.Body>
+                <Card.Title className="text-center">INICIAR SESIÓN</Card.Title>
+                <Card.Text>
+                  <Alert variant="danger">
+                    NO tienes cuenta?
+                    <Alert.Link href="/register"> REGISTRATE AQUI</Alert.Link>.
+                  </Alert>
+                  <Form>
+                    <Form.Group controlId="formBasicEmail">
+                      <Form.Label>Correo electrónico</Form.Label>
+                      <Form.Control
+                        type="email"
+                        placeholder="ejemplo@udem.co"
+                      />
+                      <Form.Text className="text-muted">
+                        Correo con el que realizó el registro.
+                      </Form.Text>
+                    </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" placeholder="Contraseña" />
-            </Form.Group>
-            <Button variant="danger" type="submit" block>
-              INGRESAR
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Contraseña</Form.Label>
+                      <Form.Control type="password" placeholder="Contraseña" />
+                    </Form.Group>
+                    <Button variant="danger" type="submit" block>
+                      INGRESAR
+                    </Button>
+                  </Form>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
--- src/Components/Pages/Register.jsx
+++ src/Components/Pages/Register.jsx
@@ -5,58 +5,73 @@ import {
   Col,
   Button,
   Form,
-  Jumbotron,
-  Image
+  Alert,
+  Card
 } from "react-bootstrap";
-import prometeo from "../../Images/prometeo.jpg";
+import udem from "../../Images/udem.jpg";
+
+var sectionStyle = {
+  width: "100%",
+  backgroundImage: `url(${udem})`,
+  backgroundRepeat: "no-repeat",
+  backgroundAttachment: "fixed",
+  backgroundSize: "cover"
+};
 
 export default function Register() {
   return (
-    <Container>
+    <div style={sectionStyle}>
       <Row>
-        <Col>
-          <Image src={prometeo} fluid />
-        </Col>
-        <Col>
-          <Jumbotron fluid>
-            <Container>
-              <h1>REGISTRO</h1>
-              <p>
-                Registrate para poder acceder y aplicar a convocatorias.
-              </p>
-              <br></br>
-              <Button href="/login" variant="outline-danger" block>
-              Ya tienes cuenta? INGRESA AQUI
-            </Button>
-            </Container>
-          </Jumbotron>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Correo electrónico</Form.Label>
-              <Form.Control type="email" placeholder="ejemplo@udem.co" />
-              <Form.Text className="text-muted">
-                Correo donde llegarán notificaciones y con el que iniciarás sesión.
-              </Form.Text>
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Nombre</Form.Label>
-              <Form.Control type="text" placeholder="Nombre" />
-            </Form.Group>
+        <Col></Col>
+        <Col sm={9} md={7} lg={5} mx="auto">
+          <Container>
+            <Card my={5}>
+              <Card.Body>
+                <Card.Title className="text-center">REGISTRO</Card.Title>
+                <Card.Text>
+                  <Alert variant="danger">
+                    Ya tienes cuenta?
+                    <Alert.Link href="/login"> INGRESA AQUI</Alert.Link>.
+                  </Alert>
+                  <Form>
+                    <Form.Group controlId="formBasicEmail">
+                      <Form.Label>Correo electrónico</Form.Label>
+                      <Form.Control
+                        type="email"
+                        placeholder="ejemplo@udem.co"
+                      />
+                      <Form.Text className="text-muted">
+                        Correo donde llegarán notificaciones y con el que
+                        iniciarás sesión.
+                      </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                      <Form.Label>Nombre</Form.Label>
+                      <Form.Control type="text" placeholder="Nombre" />
+                    </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" placeholder="Contraseña" />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Confirmar contraseña</Form.Label>
-              <Form.Control type="password" placeholder="Confimar contraseña" />
-            </Form.Group>
-            <Button variant="danger" type="submit" block>
-              REGISTRARSE
-            </Button>
-          </Form>
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Contraseña</Form.Label>
+                      <Form.Control type="password" placeholder="Contraseña" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Confirmar contraseña</Form.Label>
+                      <Form.Control
+                        type="password"
+                        placeholder="Confimar contraseña"
+                      />
+                    </Form.Group>
+                    <Button variant="danger" type="submit" block>
+                      REGISTRARSE
+                    </Button>
+                  </Form>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Container>
         </Col>
+        <Col></Col>
       </Row>
-    </Container>
+    </div>
   );
 }
