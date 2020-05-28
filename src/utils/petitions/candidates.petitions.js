4618c1843321fd198f6886dc8eaf0973ecde14b1
Miguel Vélez <34169959+mvelezg99@users.noreply.github.com>
2020-05-27 22:30:58 -0500

    Añade funcionalidad editar perfil
    - Se añade el link para acceder a la funcionalidad editar perfil
    
    - Se realizan validaciones de campos vacíos, coincidencia de contraseñas, entre otras.

--- src/utils/petitions/candidates.petitions.js
+++ src/utils/petitions/candidates.petitions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { HEADERS } from "./petitions.constants";
 
 const candidatesPetitions = axios.create({
-  baseURL: "http://173.255.216.213:3000/api/v1/"
+  baseURL: "http://173.255.216.213:3000/api/v1/",
 });
 
 const candidates = {
@@ -11,19 +11,28 @@ const candidates = {
 
     return candidatesPetitions
       .post("job/candidate", candidate, {
-        headers: { ...HEADERS, authorization: token }
+        headers: { ...HEADERS, authorization: token },
       })
-      .then(response => response.data.data);
+      .then((response) => response.data.data);
   },
   getJobCandidates(job_id) {
     const { token } = localStorage;
 
     return candidatesPetitions
       .get(`job/candidates/${job_id}`, {
-        headers: { ...HEADERS, authorization: token }
+        headers: { ...HEADERS, authorization: token },
       })
-      .then(response => response.data.data.candidates);
-  }
+      .then((response) => response.data.data.candidates);
+  },
+  editProfile(profile) {
+    const { token } = localStorage;
+
+    return candidatesPetitions
+      .put("teacher", profile, {
+        headers: { ...HEADERS, authorization: token },
+      })
+      .then((response) => response.data.data);
+  },
 };
 
 export default candidates;
